162ae822dd49bce09c0e349fefed559b02dfb76e
Lakshit Tandon <lakshittandon2004@gmail.com>
2025-06-30 09:47:40 +0530

    second

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "import.meta.env.VITE_API_BASE_URL", // 🔧 adjust if backend URL differs
+    baseUrl: "iVITE_API_BASE_URL", // 🔧 adjust if backend URL differs
     credentials: "include", // send cookies automatically
   }),
   tagTypes: ["Product", "Order", "User"],
