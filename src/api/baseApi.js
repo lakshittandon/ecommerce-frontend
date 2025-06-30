96ca604be291c9513b790c413bed57524a224fd9
Lakshit Tandon <lakshittandon2004@gmail.com>
2025-06-30 09:08:17 +0530

    Describe what you changed

--- src/api/baseApi.js
+++ src/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api/v1", // 🔧 adjust if backend URL differs
+    baseUrl: "import.meta.env.VITE_API_BASE_URL", // 🔧 adjust if backend URL differs
     credentials: "include", // send cookies automatically
   }),
   tagTypes: ["Product", "Order", "User"],
